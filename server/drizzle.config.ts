b036e6b61b22910463c10bfb935db4305afeebfe
Pwntastickev <pwntastickevin@gmail.com>
2025-09-23 16:35:31 -0600

    redpeloy

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in .env file')
+if (!process.env.POSTGRES_URL) {
+  throw new Error('POSTGRES_URL is not set in .env file')
 }
 
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
   dbCredentials: {
-    url: process.env.DATABASE_URL
+    url: process.env.POSTGRES_URL
   },
   verbose: true,
   strict: true
